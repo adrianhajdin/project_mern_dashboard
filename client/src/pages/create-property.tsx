2590244d20ccfc7ee436651af945858e2f84544e
adrianhajdin <adrianhajdin00@gmail.com>
2023-02-09 16:51:19 +0100

    update final code

--- client/src/pages/create-property.tsx
+++ client/src/pages/create-property.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import { useGetIdentity } from '@pankod/refine-core';
 import { FieldValues, useForm } from '@pankod/refine-react-hook-form';
-import { useNavigate } from '@pankod/refine-react-router-v6';
 
 import Form from 'components/common/Form';
 
 const CreateProperty = () => {
-  const navigate = useNavigate();
   const { data: user } = useGetIdentity();
   const [propertyImage, setPropertyImage] = useState({ name: '', url: '' });
   const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
